0134f9385945228d3223836803016348c33f7718
João Victor <jv05@outlook.pt>
2025-01-14 22:57:23 -0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,15 @@
 import React from "react";
 import "bootswatch/dist/flatly/bootstrap.css";
-
 import NavbarItem from "./navbarItem";
 
 function Navbar(props) {
   return (
-<<<<<<< HEAD
     <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
       <div className="container">
         <a href="/" className="navbar-brand">
           SGH
         </a>
-=======
-    <div className='navbar navbar-expand-lg fixed-top navbar-dark bg-primary'>
-      <div className='container'>
-        <a href='/' className='navbar-brand'>
-          SGH
-        </a> 
->>>>>>> cfe5ef7a63be80941d0248afcbfdddcbc42b9ce8
+
         <button
           className="navbar-toggler"
           type="button"
@@ -29,124 +21,22 @@ function Navbar(props) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-<<<<<<< HEAD
-        <ul className="navbar-nav">
-          <NavbarItem
-            render="true"
-            href="/listagem-funcionarios"
-            label="Funcionários"
-          />
-        </ul>
-        <ul className="navbar-nav">
-          <NavbarItem
-            render="true"
-            href="/listagem-tiposFuncionarios"
-            label="Tipos de Funcionarios"
-          />
-        </ul>
-        <ul className="navbar-nav">
-          <NavbarItem
-            render="true"
-            href="/listagem-pacientes"
-            label="Pacientes"
-          />
-        </ul>
-        <ul className="navbar-nav">
-          <NavbarItem
-            render="true"
-            href="/listagem-responsavelLegal"
-            label="Responsável Legal"
-          />
-        </ul>
-        <ul className="navbar-nav">
-          <NavbarItem
-            render="true"
-            href="/listagem-prontuarios"
-            label="Prontuários"
-          />
-        </ul>
+
         <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav">
-=======
-        <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-funcionarios'
-              label='Funcionários'
-            />
+          <ul className="navbar-nav mr-auto">
+            <NavbarItem render={true} href="/listagem-funcionarios" label="Funcionários" />
+            <NavbarItem render={true} href="/listagem-tiposFuncionarios" label="Tipos de Funcionários" />
+            <NavbarItem render={true} href="/listagem-pacientes" label="Pacientes" />
+            <NavbarItem render={true} href="/listagem-responsavelLegal" label="Responsável Legal" />
+            <NavbarItem render={true} href="/listagem-prontuarios" label="Prontuários" />
+            <NavbarItem render={true} href="/listagem-medicamentos" label="Medicamentos" />
+            <NavbarItem render={true} href="/listagem-lotes" label="Lotes" />
+            <NavbarItem render={true} href="/listagem-categoriaMedicamentos" label="Categoria de Medicamentos" />
+            <NavbarItem render={true} href="/listagem-agendamentos" label="Agendamentos" />
           </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-tiposFuncionarios'
-              label='Tipos de Funcionarios'
-            />
-          </ul>
-        <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-pacientes'
-              label='Pacientes'
-            />
-          </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-responsavelLegal'
-              label='Responsável Legal'
-            />
-          </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-prontuarios'
-              label='Prontuários'
-            />
-          </ul>
-        <div className='collapse navbar-collapse' id='navbarResponsive'>
-          <ul className='navbar-nav'>
->>>>>>> cfe5ef7a63be80941d0248afcbfdddcbc42b9ce8
-            <NavbarItem
-              render="true"
-              href="/listagem-medicamentos"
-              label="Medicamentos"
-            />
-          </ul>
-          <ul className="navbar-nav">
-            <NavbarItem render="true" href="/listagem-lotes" label="Lotes" />
-          </ul>
-          <ul className="navbar-nav">
-            <NavbarItem
-              render="true"
-              href="/listagem-categoriaMedicamentos"
-              label="Categoria de Medicamentos"
-            />
-          </ul>
-          <ul className="navbar-nav">
-            <NavbarItem
-              render="true"
-              href="/listagem-funcionarios"
-              label="Funcionarios"
-            />
-            <NavbarItem href="/listagem-agendamentos" label="Agendamentos" />
-          </ul>
-<<<<<<< HEAD
-          <ul className="navbar-nav">
-            <NavbarItem render="true" href="/login" label="Entrar" />
-=======
-          <ul className='navbar-nav'>
-            <NavbarItem
-              render='true'
-              href='/listagem-agendamentos'
-              label='Agendamentos'
-            />
-          </ul>
-          <ul className='navbar-nav'>
-            <NavbarItem render='true' href='/login' label='Entrar' />
->>>>>>> cfe5ef7a63be80941d0248afcbfdddcbc42b9ce8
-          </ul>
-          <ul className="navbar-nav">
-            <NavbarItem render="true" href="/" label="Sair" />
+          <ul className="navbar-nav ml-auto">
+            <NavbarItem render={true} href="/login" label="Entrar" />
+            <NavbarItem render={true} href="/" label="Sair" />
           </ul>
         </div>
       </div>
