20ef0a0d7706941e28af5e1f5868d8c5b2cd1876
João Victor <jv05@outlook.pt>
2025-02-16 18:26:36 -0300

    Delete src directory

--- src/config/axios.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://my-json-server.typicode.com/jvves/jsonfake';
-            // funcionarios, tiposFuncionarios, medicamentos, tiposMedicamentos, lotes
-export const BASE_URL2 = 'https://my-json-server.typicode.com/jvves/jsonfake2';
-            // pacientes, prontuarios, 
-
-
-//'https://my-json-server.typicode.com/marcoaparaujo/jsonfake';
